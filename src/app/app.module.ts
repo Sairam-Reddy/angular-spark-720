e9c047251bb86738f597b4669ae302258d2bfc64
Sairam-Reddy <sairamreddyg33@gmail.com>
2020-09-14 12:28:15 +0530

    2 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { FormsModule } from '@angular/forms';
 import '@progress/kendo-ui';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { CreateWorkflowModule } from '../common/workflow/create/create-workflow.module';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, MatToolbarModule, CreateWorkflowModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
