cd9c0bf54d80f82f516236dbbcc8649ab1bf9daf
Sairam-Reddy <sairamreddyg33@gmail.com>
2020-09-14 12:25:30 +0530

    2 files modified, 3 added

---
+++ src/common/workflow/draft/draft-worklfow.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import "@progress/kendo-ui";
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { CreateWorkflowComponent } from "./create-workflow.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatButtonModule } from "@angular/material/button";
+import {MatDividerModule} from '@angular/material/divider';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatDividerModule
+  ],
+  declarations: [CreateWorkflowComponent],
+  exports: [CreateWorkflowComponent]
+})
+export class DraftWorkflowModule {}
